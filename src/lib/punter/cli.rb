aae7b570f2a883a99f0ab21b5e676bc051c314a2
kumojima <kumojima@gmail.com>
2017-08-05 18:40:13 +0900

    lamduct での動作に合わせた

--- src/lib/punter/cli.rb
+++ src/lib/punter/cli.rb
@@ -2,45 +2,60 @@ require "json"
 
 module Punter
   class Cli
+    def log(str)
+      @log ||= File.open("/tmp/aaa.log", "a")
+      @log.puts str
+    end
+
     def output(hash)
-      puts hash.to_json
+      hash["state"] = @state if @state
+      json = hash.to_json
+      log(json)
+      print "#{json.length}:#{json}"
     end
 
     def input
-      JSON.parse(gets)
+      n = ""
+      d = ""
+      loop do
+        c = STDIN.getc
+        if c =~ /\d/
+          n << c
+        else
+          n = n.to_i
+          break
+        end
+      end
+      n.times { d << STDIN.getc}
+      log(d)
+      r = JSON.parse(d)
+      @state = r["state"] || {}
+      r
     end
 
     def run
       output({ me: "yarunee" })
       input # you: "yarunee"
 
-      init_hash = input
-      # punter: integer,
-      # punters: integer,
-      # map: {
-      #   sites: [integer, ...],
-      #   revers: [ { source: integer, target: integer }, ...],
-      #   mines: [integer, ...]
-      # }
-      my_punter = init_hash["punter"]
-      rivers = init_hash["map"]["rivers"]
-      output({ ready: my_punter })
-
-      loop do
-        hash = input
-        # move or stop: {
-        #   moves: [
-        #     { claim or pass: { punter: integer, source: integer, target: integer }}, ...
-        #   ]
-        # }
-        unless hash["stop"].nil?
-          puts hash["stop"]["scores"]
-          break
-        end
-        river = rivers.sample
-        river["punter"] = my_punter
-        output({ claim: river })
+      hash = input
+      case
+      when hash["stop"]
+      when hash["punter"]
+        @state["punter"] = hash["punter"]
+        @state["punters"] = hash["punters"]
+        @state["sites"] = hash["map"]["sites"]
+        @state["rivers"] = hash["map"]["rivers"]
+        @state["mines"] = hash["map"]["mines"]
+        output({ ready: @state["punter"] })
+      when hash["move"]
+        solve
       end
+    end
+
+    def solve
+      river = @state["rivers"].sample
+      river["punter"] = @state["punter"]
+      output({ move: { claim: river }})
     end
   end
 end
