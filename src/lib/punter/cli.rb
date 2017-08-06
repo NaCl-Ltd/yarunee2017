a9fed8cf1b54693f04406ba762cab602b133b549
kumojima <kumojima@gmail.com>
2017-08-06 22:15:35 +0900

    各mineからの距離を計算するよう修正

--- src/lib/punter/cli.rb
+++ src/lib/punter/cli.rb
@@ -65,6 +65,10 @@ module Punter
         end
         @state["mines"] = hash["map"]["mines"]
         @state["my_sites"] = @state["mines"]
+        calc_site_distance
+        @state["sites_distances"] = @sites
+        @state["mine_routes"] = @state["mines"].map { |i| [i, [i]] }.to_h
+        @state["connect"] = true
         output({ ready: @state["punter"] })
       when hash["move"]
         hash["move"]["moves"].each do |move|
@@ -78,6 +82,16 @@ module Punter
               %w(source target).each do |label|
                 @state["my_sites"].unshift m[label] unless @state["my_sites"].include?(m[label])
               end
+              if (f = fetch_site_to_mine(m["source"])).nil?
+                f = fetch_site_to_mine(m["target"])
+                new_site = m["source"]
+              else
+                new_site = m["target"]
+              end
+              @state["mine_routes"][f.to_s] << new_site
+              if @state["mines"].include?(new_site)
+                @state["connect"] = false
+              end
             end
           end
         end
@@ -86,14 +100,28 @@ module Punter
     end
 
     def solve
-      calc_site_distance
       source = nil
       target = nil
       @state["my_sites"].each do |site|
         targets = @state["rivers"][site.to_s]
         if targets && !targets.empty?
           source = site
-          target = targets.min_by { |i| @sites[i] }
+          mine = fetch_site_to_mine(site)
+          if @state["connect"]
+            target = targets.min_by do |i|
+              @state["sites_distances"].select do |k,v|
+                k.to_s != mine.to_s
+              end.map do |k,v|
+                v[i.to_s]
+              end.flatten.min
+            end
+          else
+            target = targets.max_by do |i|
+              @state["sites_distances"].map do |k,v|
+                v[i.to_s]
+              end.flatten.min
+            end
+          end
           break
         end
       end
@@ -108,25 +136,34 @@ module Punter
       end
     end
 
-    # 各siteが残りの川を使って行ける最短距離を求める
+    # 各mineごとに各siteへの距離を計算する
     def calc_site_distance
       @sites = {}
-      @state["sites"].each do |site|
-        @sites[site] = 99999999
-      end
-      @state["mines"].each { |i| @sites[i] = 0 }
-      q = @state["mines"].map { |i| [i, 0] }
-      q.each do |site, d|
-        targets = @state["rivers"][site.to_s]
-        if targets
-          targets.each do |target|
-            if @sites[target] > d + 1
-              @sites[target] = d + 1
-              q << [target, d + 1]
+      @state["mines"].each do |mine|
+        sites = {}
+        @state["sites"].each do |site|
+          sites[site] = 99999999
+        end
+        sites[mine] = 0
+        q = [[mine, 0]]
+        q.each do |site, d|
+          targets = @state["rivers"][site.to_s]
+          if targets
+            targets.each do |target|
+              if sites[target] > d + 1
+                sites[target] = d + 1
+                q << [target, d + 1]
+              end
             end
           end
         end
+        @sites[mine] = sites
       end
+    end
+
+    # site id からどのmine(id)由来の道か返す
+    def fetch_site_to_mine(site)
+      @state["mine_routes"].select { |k,v| v.include?(site) }.keys.first&.to_i
     end
   end
 end
